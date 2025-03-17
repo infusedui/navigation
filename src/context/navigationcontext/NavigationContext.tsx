2099aea2f2983b0d95e243ee9d0b1a6edd718fc9
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-03-17 13:52:59 +0100

    Update NavigationContext.tsx

--- src/context/navigationcontext/NavigationContext.tsx
+++ src/context/navigationcontext/NavigationContext.tsx
@@ -1,13 +1,22 @@
 import { createContext, useState } from "react";
-import { ContextProps } from "./NavigationContext.types";
+import { ContextProps, ProviderProps } from "./NavigationContext.types";
 
-export const NavigationContext = createContext({});
+export const NavigationContext = createContext<ProviderProps>({
+  appActualPage: "",
+  setAppActualPage: () => {},
+  appActualSubPage: "",
+  setAppActualSubPage: () => {},
+  appActualCategory: "",
+  setAppActualCategory: () => {},
+  appMenuVisibility: false,
+  setAppMenuVisibility: () => {},
+});
 
 const NavigationProvider: React.FC<ContextProps> = ({ children }) => {
-  const [appActualPage, setAppActualPage] = useState<String>("");
-  const [appActualSubPage, setAppActualSubPage] = useState<String>("");
-  const [appActualCategory, setAppActualCategory] = useState<String>("");
-  const [appMenuVisibility, setAppMenuVisibility] = useState<Boolean>(false);
+  const [appActualPage, setAppActualPage] = useState<string>("");
+  const [appActualSubPage, setAppActualSubPage] = useState<string>("");
+  const [appActualCategory, setAppActualCategory] = useState<string>("");
+  const [appMenuVisibility, setAppMenuVisibility] = useState<boolean>(false);
 
   return (
     <NavigationContext.Provider
